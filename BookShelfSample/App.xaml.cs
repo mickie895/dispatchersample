70aa7f64dd8a54b592527dc4e179c51bf100d91d
Mickie <mken.mickie@gmail.com>
2021-12-05 11:31:37 +0900

    プロジェクト ファイルを追加します。

---
+++ BookShelfSample/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BookShelfSample.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace BookShelfSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ BookShelfSample/Model/BookInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShelfSample.Model
+{
+    class BookInfo
+    {
+        public Book book;
+        public bool hasRead;
+    }
+
+    class Book
+    {
+        public string Name;
+        public Book(string name)
+        {
+            Name = name;
+        }
+    }
+}
