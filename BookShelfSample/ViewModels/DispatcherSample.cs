4e38e39b88744f84bb443c55ed48c2cd0b36c9c3
Mickie <mken.mickie@gmail.com>
2021-12-18 20:43:47 +0900

    スレッドセーフとか関係ない危ない実装法

---
+++ BookShelfSample/ViewModels/DispatcherSample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace DispatcherSample
+{
+    public abstract class DispatcherSample
+    {
+        // https://espresso3389.hatenablog.com/entry/2016/08/09/022724 の、
+        // 「Dispatcherの闇」の解決を試みる
+
+        protected Dispatcher dispatcher = null;
+
+        public bool Initiallized => dispatcher != null;
+
+        /// <summary>
+        /// Dispatcherの初期化処理
+        /// </summary>
+        protected DispatcherSample()
+        {
+            Dispatcher baseDispatcher = Dispatcher.CurrentDispatcher;
+
+            baseDispatcher.ShutdownStarted += (s, e) =>
+                dispatcher?.BeginInvokeShutdown(DispatcherPriority.Normal);
+
+            var thread = new Thread(
+                new ThreadStart(
+                    () => {
+                        dispatcher = Dispatcher.CurrentDispatcher;
+                        baseDispatcher.BeginInvoke((Action)OnInitiallized);
+                        Dispatcher.Run();
+                    })
+                );
+            thread.Start();
+        }
+
+        protected abstract void OnInitiallized();
+    }
+}
--- BookShelfSample/ViewModels/MainWindowViewModel.cs
+++ BookShelfSample/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BookShelfSample.Model;
 using Prism.Mvvm;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BookShelfSample.ViewModels
 {
@@ -17,12 +18,22 @@ namespace BookShelfSample.ViewModels
 
         public MainWindowViewModel()
         {
-            BookList = new List<BookInfo>()
+            BookList = new List<BookInfo>();
+            AddItemToShelf();
+
+        }
+
+        public async void AddItemToShelf()
+        {
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 100; i++)
             {
-                new BookInfo("1984", true),
-                new BookInfo("動物農場"),
-                new BookInfo("すばらしい新世界", true),
-            };
+                string bookName = $"TaskName{i}";
+                tasks.Add(Task.Run(() => BookList.Add(new BookInfo(bookName))));
+            }
+
+            await Task.WhenAll(tasks);
+
         }
     }
 }
