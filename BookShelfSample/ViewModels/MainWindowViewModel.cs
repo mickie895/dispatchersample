cb4ec18862ded2ec2394ca259039ee0a52ea7f6c
Mickie <mken.mickie@gmail.com>
2021-12-18 21:40:19 +0900

    スレッドセーフ時の処理を作成

--- BookShelfSample/ViewModels/MainWindowViewModel.cs
+++ BookShelfSample/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using BookShelfSample.Model;
 using Prism.Mvvm;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace BookShelfSample.ViewModels
 {
@@ -14,25 +16,43 @@ namespace BookShelfSample.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public List<BookInfo> BookList { get; set; }
+        private List<BookInfo> bookList;
+        public List<BookInfo> BookList
+        {
+            get { return bookList; }
+            set { SetProperty(ref bookList, value); }
+        }
+
+        private List<BookInfo> books = new List<BookInfo>();
 
         public MainWindowViewModel()
         {
+            Dispatcher uiThread = Dispatcher.CurrentDispatcher;
             BookList = new List<BookInfo>();
             AddItemToShelf();
-
+            uiThread.BeginInvoke(() => {
+                BookList = books;
+                Title = $"本の総数：{BookList.Count}冊";
+            });
         }
 
         public async void AddItemToShelf()
         {
+            books = new List<BookInfo>();
+            ConcurrentQueue<BookInfo> createdBookQueue = new ConcurrentQueue<BookInfo>();
             List<Task> tasks = new List<Task>();
             for (int i = 0; i < 100; i++)
             {
                 string bookName = $"TaskName{i}";
-                tasks.Add(Task.Run(() => BookList.Add(new BookInfo(bookName))));
+                tasks.Add(Task.Run(() => createdBookQueue.Enqueue(new BookInfo(bookName))));
             }
 
             await Task.WhenAll(tasks);
+
+            while(createdBookQueue.TryDequeue(out BookInfo book))
+            {
+                books.Add(book);
+            }
 
         }
     }
